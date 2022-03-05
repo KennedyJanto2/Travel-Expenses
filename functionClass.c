e682f767dfae338f0fade6c93ac9fb3ec394bb9d
KennedyJanto2 <Kennedyjanto3@gmail.com>
2022-03-04 20:54:31 -0800

    added functions for days, car rentals, parking fees, and meal expenses

---
+++ functionClass.c
@@ -0,0 +1,34 @@
+#include greyson-functions.h
+
+void setDaysSpent(struct Trip *trip, int days){
+    trip->day.size() + 1;
+}
+
+void setCarRentals(struct Trip *trip, int price){
+    trip->rentalCost = price;
+}
+
+void setParkingFees(struct Day *day, int parkingFee){
+    day->parkingCost = parkingFee;
+}
+
+void setMealExpenses(){
+
+}
+
+int getDaysSpent(){
+    return trip->day;
+}
+
+float getCarRentals(){
+    return trip->rentalCost;
+}
+
+float getParkingFees(){
+    return day->parkingCost;
+}
+
+float getMealExpenses(){
+
+}
+
