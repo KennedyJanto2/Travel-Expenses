4f82e4383f3429969e1b6e1a2014c311fc21a4df
KennedyJanto2 <Kennedyjanto3@gmail.com>
2022-03-04 21:14:42 -0800

    added functions

--- functionClass.c
+++ functionClass.c
@@ -12,8 +12,12 @@ void setParkingFees(struct Day *day, int parkingFee){
     day->parkingCost = parkingFee;
 }
 
-void setMealExpenses(){
+void setRegistrationFees(struct Trip *trip, int registrationFee){
+    trip->eventCost = registrationFee;
+}
 
+void setMealExpenses(struct Day *day, int mealCost){
+    day->mealExpenses = mealCost;
 }
 
 int getDaysSpent(){
@@ -28,7 +32,11 @@ float getParkingFees(){
     return day->parkingCost;
 }
 
-float getMealExpenses(){
+float getRegistrationFees(){
+    return day->evenCost;
+}
 
+float getMealExpenses(){
+    return day-> mealExpenses;
 }
 
