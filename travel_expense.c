e822e17cdaf821ce8f70a2b99923f9ca0bd47727
“Liangjie <liangjieshen@cpp.edu>
2022-03-05 14:58:09 -0800

    update

--- travel_expense.c
+++ travel_expense.c
@@ -6,6 +6,8 @@ Liangjie Shen
 
 #include <stdio.h> // input/output library
 #include <string.h> // string library including strlen()
+#include <ctype.h> // isdigit()
+#include <stdbool.h> // boolean function
 
 // function declaration
 bool timeFormat(char[]);
@@ -26,23 +28,59 @@ int main()
             printf("Number of days cannot be less than 1! Try again!\n");
     }
 
-    while(timeFormat(departTime)){
+    while(!timeFormat(departTime)){
         printf("The time of departure on the first day of the trip:[##:## AM/PM] ");
-        scanf("%s", &departTime);
+        scanf("%s",departTime);
+
+        if(!timeFormat(departTime))
+            printf("Wrong depart time format!\n");
         
     }
 
-    while(timeFormat(arrivalTime)){
+    while(!timeFormat(arrivalTime)){
         printf("The time of arrival back home on the last day of the trip:[##:## AM/PM] ");
-        scanf("%s", &arrivalTime);
+        scanf("%s",arrivalTime);
+        if(!timeFormat(arrivalTime))
+            printf("Wrong arrive time format!\n");
     }
-
-
-
-
 }
 
 bool timeFormat(char validTime[]){
-    for(int i = 0; i < validTime.)
-    if(validTime[0] >= '0' && validTime[0] <='9' && validTime[1] == ':' && validTime )
+    int counter = 0;
+    if(isdigit(validTime[counter])){ 
+
+        if(validTime[counter] == '1' && isdigit(validTime[counter+1])){
+            counter++;
+            if(validTime[counter] >= '0' && validTime[counter] <='9'){
+                counter++;
+            }
+        }
+
+        else if(validTime[counter] >= '0' && validTime[counter] <='9')
+            counter++;
+
+        else    
+            return false;
+
+        if(validTime[counter] == ':'){
+            counter++;
+            if(isdigit(validTime[counter])&& isdigit(validTime[counter+1])){
+                if(validTime[counter] >= '0' && validTime[counter] <='9' && validTime[counter+1] >= '0' && validTime[counter+1] <='9'){
+                    counter+=2;
+                    if(validTime[counter] == ' '){
+                        counter++;
+                        if((validTime[counter] == 'A' || validTime[counter] == 'P') && validTime[counter+1] == 'M')
+                            return true;
+
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    return false;
+
+
 }
